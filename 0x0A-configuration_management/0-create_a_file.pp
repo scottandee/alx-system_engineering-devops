8a8ed2d8b330c9c167f93c4fdc433f2eb07cff46
Andee <andeeolayinkascott@gmail.com>
2023-06-23 13:06:32 +0000

    Create file using the 'file' resource

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This script creates a file with name 'school'
+# in the /tmp directory
+
+
+file { '/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
