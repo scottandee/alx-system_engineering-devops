74139cfa96b1b7fd9e48aa162a3f11830cb1d639
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-25 16:23:48 +0100

    Reduce number of queries to url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,17 +13,19 @@ if __name__ == "__main__":
     details = res.json()
     emp_name = details.get("name")
 
-    # Get a list of done tasks, find the length
-    res = requests.get(f"""https://jsonplaceholder.typicode.com/\
-todos?userId={emp_id}&completed=true""")
-    completed = res.json()
-    num_of_done = len(completed)
-
     # Get the total number of tasks done
-    res = requests.get(f"""https://jsonplaceholder.typicode.com/\
-todos?userId={emp_id}""")
+    res = requests.get(f"https://jsonplaceholder.typicode.com/\
+todos?userId={emp_id}")
     all_tasks = res.json()
     tots_num_of_tasks = len(all_tasks)
+
+    # Count number of completed tasks
+    num_of_done = 0
+    completed = []
+    for task in all_tasks:
+        if task.get("completed") is True:
+            num_of_done = num_of_done + 1
+            completed.append(task)
 
     print(f"Employee {emp_name} is done with tasks({num_of_done}\
 /{tots_num_of_tasks}):")
