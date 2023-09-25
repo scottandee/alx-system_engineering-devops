f97ce47e33dcd6b29a76b860d0034f64d158b759
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-25 20:20:03 +0100

    Add double quotes

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,13 +17,12 @@ if __name__ == "__main__":
 
     todos = requests.get(todos_url).json()
     # data is the dictionary that will be passed to the csv writer
-    data = {"id": id, "username": username, "completed": "", "title": ""}
 
     with open("{}.csv".format(id), mode="w") as csv_file:
-        fieldnames = data.keys()
-        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         for task in todos:
-            data["completed"] = task.get("completed")
-            data["title"] = task.get("title")
-            csv_writer.writerow(data)
+            completed = task.get("completed")
+            title = task.get("title")
+            row = [id, username, completed, title]
+            csv_writer.writerow(row)
